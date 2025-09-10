5e1b6deccafb8ef39172e2089998e6215d703717
zlfn <ung@zlfn.space>
2025-09-11 02:10:07 +0900

    feat: initial commit

---
+++ 0_setup/msvc/build.ps1
@@ -0,0 +1,16 @@
+$currentDir = Get-Location
+if (Test-Path build) {
+    Remove-Item -Recurse -Force build
+}
+New-Item -ItemType Directory -Path build
+
+Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Community" -DevCmdArguments "-arch=amd64 -host_arch=amd64"
+
+Set-Location $currentDir
+
+cl testbed.cpp /EHsc /std:c++17 /D "NDEBUG" /I ..\..\include /Fo"build\\" /Fe"build\testbed.exe" /Fd"build\vc.pdb" /link /LIBPATH:..\..\win-x64-msvc\lib freeglut.lib glew32.lib opengl32.lib
+$env:Path = $env:Path + ";$currentDir\..\..\win-x64-msvc\bin"
+Write-Host "Testbed built successfully. Running..."
+.\build\testbed.exe
+Write-Host "Testbed exited."
---
+++ 0_setup/msvc/testbed.cpp
@@ -0,0 +1,46 @@
+﻿#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    glColor3f(0, 1, 1);
+    glVertex3f(0, 1, 0);
+    glColor3f(0, 1, 1);
+    glVertex3f(-1, -1, 0);
+    glColor3f(0, 1, 1);
+    glVertex3f(1, -1, 0);
+    glEnd();
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+void timer(int value) {
+    glutPostRedisplay();
+    glutTimerFunc(16, timer, 0);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("FreeGLUT + GLEW + GLM Example");
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "GLEW 초기화 실패: " << glewGetErrorString(err) << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
+
+    glutMainLoop();
+    return 0;
+}
