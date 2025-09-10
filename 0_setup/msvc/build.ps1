48ac6a75312817f7da6ac9834c5d7e289bb5165b
zlfn <ung@zlfn.space>
2025-09-11 03:49:57 +0900

    fix: wrong include directory

--- 0_setup/msvc/build.ps1
+++ 0_setup/msvc/build.ps1
@@ -9,7 +9,7 @@ Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\C
 
 Set-Location $currentDir
 
-cl testbed.cpp /EHsc /std:c++17 /D "NDEBUG" /I ..\..\include /Fo"build\\" /Fe"build\testbed.exe" /Fd"build\vc.pdb" /link /LIBPATH:..\..\win-x64-msvc\lib freeglut.lib glew32.lib opengl32.lib
+cl testbed.cpp /EHsc /std:c++17 /D "NDEBUG" /I ..\..\win-x64-msvc\include /Fo"build\\" /Fe"build\testbed.exe" /Fd"build\vc.pdb" /link /LIBPATH:..\..\win-x64-msvc\lib freeglut.lib glew32.lib opengl32.lib
 $env:Path = $env:Path + ";$currentDir\..\..\win-x64-msvc\bin"
 Write-Host "Testbed built successfully. Running..."
 .\build\testbed.exe
