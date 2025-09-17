340afaf0a743899cb0f1964372923da83476e340
Hyunseong <hyunseong3020@naver.com>
2025-09-17 11:19:13 +0900

    feat: matrix transformation (#18)
    remove unnecessary functions & make all vector addition to matrix
    multiplication
    
    ---------
    
    Co-authored-by: zlfn <ung@zlfn.space>

--- 1_2d_game/src/utils.hpp
+++ 1_2d_game/src/utils.hpp
@@ -10,50 +10,6 @@ int getRandomRange(int a, int b) { // a, b 포함
     static std::uniform_int_distribution<int> dist(a, b);
     return dist(gen);
 }
-void drawCircle(glm::fvec2 center, float radius, int numSegments, glm::fvec3 color) {
-    glColor3f(color.x, color.y, color.z);
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(center.x, center.y);
-    for (int i = 0; i <= numSegments; i++) {
-        float angle = static_cast<float>(2.0f * std::numbers::pi * i / numSegments);
-        float x = center.x + radius * std::cos(angle);
-        float y = center.y + radius * std::sin(angle);
-        glVertex2f(x, y);
-    }
-    glEnd();
-}
-
-void drawRect(glm::fvec2 center, float size, glm::fvec3 color) {
-    float half = size / 2.0f;
-
-    glColor3f(color.x, color.y, color.z);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(center.x - half, center.y + half);
-    glVertex2f(center.x - half, center.y - half);
-    glVertex2f(center.x + half, center.y - half);
-    glVertex2f(center.x - half, center.y + half);
-    glVertex2f(center.x + half, center.y - half);
-    glVertex2f(center.x + half, center.y + half);
-    glEnd();
-}
-
-void drawTriangle(glm::fvec2 center, float size, glm::fvec3 color) {
-    glColor3f(color.x, color.y, color.z);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(center.x, center.y + size / 2);
-    glVertex2f(center.x - size / 2, center.y - size / 2);
-    glVertex2f(center.x + size / 2, center.y - size / 2);
-    glEnd();
-}
-
-void drawTriangle(glm::fvec2 center, float size, glm::fvec4 color) {
-    glColor4f(color.x, color.y, color.z, color.w);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(center.x, center.y + size / 2);
-    glVertex2f(center.x - size / 2, center.y - size / 2);
-    glVertex2f(center.x + size / 2, center.y - size / 2);
-    glEnd();
-}
 
 void drawSpaceship(glm::fvec2 center, float size, glm::fvec4 color) {
     // Enable lighting for 3D effect
